b5038efa71404de2db52d5f3858ec2be91b3afd5
Jackson Eckert <jaxon.eckert@gmail.com>
2024-09-24 22:45:21 -0500

    Solve Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const isNumber = (val) => typeof val === "number";
+const isInteger = (num) => Math.floor(num) === num;
 
+const sumAll = function (a, b) {
+  if (
+    a < 0 ||
+    b < 0 ||
+    !isNumber(a) ||
+    !isNumber(b) ||
+    !isInteger(a) ||
+    !isInteger(b)
+  ) {
+    return "ERROR";
+  }
+
+  const lower = a < b ? a : b;
+  const upper = a < b ? b : a;
+  let sum = 0;
+
+  for (let i = lower; i <= upper; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
