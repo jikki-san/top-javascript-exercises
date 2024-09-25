cff12054e4bd832c38dae932b0aefac17ac08243
Jackson Eckert <jaxon.eckert@gmail.com>
2024-09-24 22:25:57 -0500

    Solve Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (stringToRepeat, numTimes) {
+  if (numTimes < 0) {
+    return "ERROR";
+  }
 
+  let result = "";
+
+  for (let i = 0; i < numTimes; i++) {
+    result += stringToRepeat;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
