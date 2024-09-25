7ae050e6672242c032051d653c17d861b84523d9
Jackson Eckert <jaxon.eckert@gmail.com>
2024-09-24 22:29:15 -0500

    Solve Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (stringToReverse) {
+  let result = "";
 
+  for (let i = stringToReverse.length - 1; i >= 0; i--) {
+    result += stringToReverse[i];
+  }
+
+  return result;
 };
 
 // Do not edit below this line
