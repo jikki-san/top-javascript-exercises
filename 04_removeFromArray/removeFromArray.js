42e53608d0834e2b3942c79e932dc9b345d8df9c
Jackson Eckert <jaxon.eckert@gmail.com>
2024-09-24 22:38:14 -0500

    Solve Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...rest) {
+  for (const element of rest) {
+    arr = arr.filter((el) => el !== element);
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
